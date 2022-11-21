2495c80ca6806cd05f3a4e93a0f5aa97042bb39b
lucaF <lucafiscariello@gmail.com>
2022-11-21 20:41:35 +0000

    Update 3 files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
+
+
+
 
 const Home = () => {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+        axios.post("http://localhost:8080/",{response})
+
+    }
+    
 
     return (
         <>
             <h1>Hello, World!</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Aliquid, reiciendis iusto accusantium necessitatibus veniam nostrum totam consequuntur, labore repudiandae, deserunt temporibus amet sint unde ad enim ducimus earum explicabo debitis.
-            </p>
+            <GoogleLogin
+                clientId="394295198428-icelu33b5ulfasp4i0anf24gsltjmnje.apps.googleusercontent.com"
+                buttonText="Accedi"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+                responseType="code"
+                accessType="offline"
+                scope="openid email profile https://www.googleapis.com/auth/calendar"   
+            />
         </>
     );
 };
